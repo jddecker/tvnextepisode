a185958af344194a4547d8d54b94d3e16635e9f2
Jeffrey Decker <jeffreydaviddecker@gmail.com>
2017-12-17 15:22:51 -0800

    Split out into functions

--- tvshows.py
+++ tvshows.py
@@ -2,19 +2,13 @@
 
 import requests
 
-
-def main():
-    """Run when the program starts"""
-    query = input('Show search: ')
-    parameters = {'q': query}
-    response = requests.get('http://api.tvmaze.com/search/shows', params=parameters)
-    shows = response.json()
+def get_show_list(shows):
+    """Gather shows data into a dictionary from json data"""
     result_num = 0
+    results = {}
     for show in shows:
         show = show['show']
         result_num += 1
-        spacing = ' '
-        spacing *= 3 - len(str(result_num))
         show_name = show['name']
         if show['network'] is not None:
             network_name = show['network'].get('name')
@@ -22,15 +16,39 @@ def main():
             network_name = show['webChannel'].get('name')
         else:
             network_name = None
-        if network_name is None:
-            network_name = ''
         if show['premiered'] is None:
-            premiered_year = ''
+            premiered_year = None
         else:
             premiered_year = show['premiered'][:4]
-        show_desc = ', '.join([network_name, premiered_year])
+        result_tuple = (show_name, network_name, premiered_year)
+        results[result_num] = result_tuple
+    return results
 
-        print(str(result_num) + '.' + spacing + show_name + ' (' + show_desc + ')')
+def print_shows(results):
+    """Print the results of the shows"""
+    for num in results:
+        if len(str(num)) == 1:
+            number = ' ' + str(num)
+        else:
+            number = str(num)
+        name = results[num][0]
+        desc = []
+        if results[num][1] is not None:
+            desc.append(results[num][1])
+        if results[num][2] is not None:
+            desc.append(results[num][2])
+        desc_text = ', '.join(desc)
+        print(number + '. ' + name + ' (' + desc_text + ')')
+
+def main():
+    """Run when the program starts"""
+    query = input('Show search: ')
+    parameters = {'q': query}
+    api = 'http://api.tvmaze.com/search/shows'
+    response = requests.get(api, params=parameters)
+    shows = response.json()
+    results = get_show_list(shows)
+    print_shows(results)
 
 if __name__ == '__main__':
     main()
