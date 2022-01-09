d3a25728cdd45c82e8726711a516e97eada1ab19
Jeffrey David Decker <jeffreydaviddecker@gmail.com>
2022-01-08 17:09:31 -0800

    Added Get-Help information

--- tvnextepisode.ps1
+++ tvnextepisode.ps1
@@ -1,28 +1,43 @@
 <#
-    Querying TVMaze's API to get when the next episode of a show will air
+    .SYNOPSIS
+        Find out when the next episode of a TV show
+    .DESCRIPTION
+        Querying TVMaze's API to get when the next episode of a show will be released
+
+        TVMaze website: https://tvmaze.com
+        TVMaze API: https://api.tvmaze.com
+    .PARAMETER Show
+        The name of the show to query
+    .INPUTS
+        None. Cannot pipe objects to tvnextepisode.ps1.
+    .OUTPUTS
+        Formatted string with the show results, show premiered date, and if there is a next episode scheduled and when
+    .EXAMPLE
+        PS> .\tvnextepisode.ps1 -Show "what we do in the shadows"
+    .LINK
+        https://github.com/jddecker/tvnextepisode
 #>
 
 param (
     [Parameter(Mandatory)][string] $Show  # What show to query
 )
 
-@"
-Find out when the next episode of a TV show
-Information provided by TVmaze.com <https://tvmaze.com>
-
-"@
-
-# Setting up search variables
+# Query
 $search = @{
     q = $Show;
     embed = "nextepisode"
 }
 
-$response = Invoke-RestMethod -Uri "https://api.tvmaze.com/singlesearch/shows" -Method "Get" -Body $search
+try {
+    $response = Invoke-RestMethod -Uri "https://api.tvmaze.com/singlesearch/shows" -Method "Get" -Body $search
+} catch {
+    $_.Exception  # Output error
+}
 
+$name = "$($response.name) ($(Get-Date $response.premiered -Format 'yyyy'))"
 # If no next episode air stamp then no new episode
 if ($null -eq $response._embedded.nextepisode.airstamp) {
-    "No new episodes of $($response.name) at this time"
+    "No new episodes of $name at this time"
 } else {
     # Getting timezone name
     if ((Get-Date).IsDaylightSavingTime()) {
@@ -30,5 +45,7 @@ if ($null -eq $response._embedded.nextepisode.airstamp) {
     } else {
         $tzname = (Get-Timezone).StandardName
     }
-    "The next episode of $($response.name) ($(Get-Date $response.premiered -Format 'yyyy')) is $(Get-Date $response._embedded.nextepisode.airstamp -Format 'MM/dd/yyyy @ h:mm tt K') ($($tzname))"
+    "The next episode of $name is $(Get-Date $response._embedded.nextepisode.airstamp -Format 'dddd MM/dd/yyyy @ h:mm tt K') ($($tzname))"
 }
+
+"Results from TVmaze.com <https://tvmaze.com>"
